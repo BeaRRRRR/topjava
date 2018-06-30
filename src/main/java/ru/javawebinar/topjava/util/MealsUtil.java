9fcb6e86d0b57bc569203442fedf0bf7c78b83f9
BeaRRRRR <mishanx873@gmail.com>
2018-06-30 14:49:58 +0300

    refactored

---
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -0,0 +1,38 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MealsUtil {
+    public static void main(String[] args) {
+        List<Meal> mealList = Arrays.asList(
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+        );
+        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+//        .toLocalDate();
+//        .toLocalTime();
+    }
+
+    public static List<MealWithExceed>  getFilteredWithExceeded(List<Meal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        Map<LocalDate, Integer> caloriesMap = mealList.stream()
+                .collect(Collectors.groupingBy(e -> e.getDateTime().toLocalDate(), Collectors.summingInt(Meal::getCalories)));
+        return mealList.stream()
+                .filter(e -> TimeUtil.isBetween(e.getDateTime().toLocalTime(), startTime, endTime))
+                .map(e -> (new MealWithExceed(e.getDateTime(), e.getDescription(), e.getCalories(), caloriesMap.get(e.getDateTime().toLocalDate()) > caloriesPerDay)))
+                .collect(Collectors.toList());
+    }
+}
