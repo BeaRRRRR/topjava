6fcfcad7eb54cfeee6c9e97b21c8aba57b9674c9
BeaRRRRR <mishanx873@gmail.com>
2018-07-24 13:36:28 +0300

    meal repo using jdbc(postgreSQL)

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceImplTest.java
@@ -0,0 +1,50 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.List;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceImplTest {
+
+    @Autowired
+    private MealService mealService;
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getBetweenDateTimes() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void create() {
+        List<Meal> all = mealService.getAll(100000);
+        all.forEach(System.out::println);
+    }
+}
